7d1caa73318420a97d00593af08353baa93f85d0
Matthew Redenius <matt-mac@matthews-mini.fios-router.home>
2017-05-23 18:57:58 -0700

    Added in backend functionality

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: Properties
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        nameTextField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
+    //MARK: Actions
+    
+    @IBAction func defaultLabelButton(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+    
 }
 
